9e1fbf2b11a5a85590edc4f1b335b06a736e3027
ivela@CompuElias <ivelasque@fwdcostarica.com>
2024-11-11 15:41:55 -0600

    APP-001 T12

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -8,24 +8,77 @@ class PartnersSerializer(serializers.ModelSerializer):
         model = Partners
         fields = '__all__'
     
+    def validate_partner_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El nombre de la alianza no puede estar vacío.")
+    def validate_partner_logo_url(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo del logo no puede estar vacío.")
+        return value
 
+##############################################################################################  
 class Courses_CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses_Category
         fields = '__all__'
-    
-           
+    def validate_category_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El nombre de la categoría no puede estar vacío.")
+        
+##############################################################################################      
 class CoursesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courses
         fields = '__all__'
         
+    def validate_course_image_url(self, value):
+        if not value:
+            raise serializers.ValidationError(" El campo de la imagen de referencia no puede estar vacío.")
+        return value
+    def validate_course_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El nombre del curso no puede estar vacío.")
+    def validate_course_description(self, value):
+        if not value:
+            raise serializers.ValidationError("La descripción del curso no puede estar vacío.")
+        if len(value) < 100:
+            raise serializers.ValidationError("La descripción de la categoría debe contener al menos 100 carácteres.")
+    def validate_course_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("El precio del curso no puede ser negativo.")
+        return value
+    def validate_course_schedule(self, value):
+        if not value:
+            raise serializers.ValidationError("El horario del curso no puede estar vacío.")
     
+    def valid_begins_ends(self, begins, ends, value):
+        if begins >= ends:
+            raise serializers.ValidationError("La fecha de finalización del curso debe ser posterior a la fecha de inicio.")
+        return value
+    def validate_course_duration(self, value):
+        if not value:
+            raise serializers.ValidationError("La duración del curso no puede estar vacía.")
+    
+#############################################################################################
 class People_InterestedSerializer(serializers.ModelSerializer):
     class Meta:
         model = People_Interested
         fields = '__all__'
+    def validate_person_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo nombre no puede estar vacío.")
+    def validate_person_first_last_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo apellido no puede estar vacío.")
+    def validate_person_email(self, value):
+        if '@' not in value:
+            raise serializers.ValidationError("El correo electrónico debe contener el símbolo '@'.")
+        return value
+    def validate_course(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo nombre del curso no puede estar vacío.")
 
+########################################################################################################
 class ProvincesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provinces
@@ -50,7 +103,12 @@ class AddressesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Addresses
         fields = '__all__'
-        
+    
+    def validate_address(self, value):
+        if not value:
+            raise serializers.ValidationError("Debes escrbir tu dirección exacta.")
+
+#############################################################################      
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
@@ -65,17 +123,46 @@ class IdentificationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Identifications
         fields = '__all__'
-     
+
+######################################################################
 class FormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Form
         fields = '__all__'
         
+    def validate_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo nombre no puede estar vacío.")
+    def validate_first_last_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo del primer apellido no puede estar vacío.")
+    def validate_second_last_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo del segundo apellido no puede estar vacío.")
+    def validate_email(self, value):
+        if '@' not in value:
+            raise serializers.ValidationError("El correo electrónico debe contener el símbolo '@'.")
+        return value
+        
 class AdministratorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Administrator
         fields = '__all__'
-        
+    def validate_admin_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo nombre no puede estar vacío.")
+    def validate_admin_first_last_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo del primer apellido no puede estar vacío.")
+    def validate_admin_second_last_name(self, value):
+        if not value:
+            raise serializers.ValidationError("El campo del segundo apellido no puede estar vacío.")
+    def validate_admin_email(self, value):
+        if '@' not in value:
+            raise serializers.ValidationError("El correo electrónico debe contener el símbolo '@'.")
+        return value
+    
+##################################################################################    
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
@@ -101,6 +188,13 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+        
+    def validate_payment_amount(self, value):
+        if value < 0:
+            raise serializers.ValidationError("El total del pago realizado no puede ser negativo.")
+        return value
+        
+    
         
 
 
